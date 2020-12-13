d53481fbed301a40fc39da39225a6cf5d4d9cfd4
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-12-13 16:34:15 +0200

    actions/weighting/sparkfun-qwiic-scale-NAU7802 > javascript > test: readings

--- actions/weighting/sparkfun-qwiic-scale-NAU7802/packages/javascript/tests/readings/index.js
+++ actions/weighting/sparkfun-qwiic-scale-NAU7802/packages/javascript/tests/readings/index.js
@@ -1,16 +1,22 @@
-const NAU7802 = require('../../distribution');
+const NAU7802 = require('../../distribution').default;
 
 
 
 const main = async () => {
-    const address = 0x35;
+    const address = 0x38;
     const scale = new NAU7802(address);
     scale.begin(1);
 
     while(true) {
-        const reading = scale.getReading();
+        const reading = await scale.getReading();
 
         console.log('reading', reading);
+
+        await new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(true);
+            }, 500);
+        });
     }
 }
 
